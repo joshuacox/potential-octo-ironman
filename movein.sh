ece6a81b3924597bfb76f79715df153a6cc9e0ea
Josh Cox <josh@webhosting.coop>
2014-11-24 17:16:20 -0600

    initial import

---
+++ movein.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# move in to oltorf
+hostname octo.webhosting.coop
+echo octo.webhosting.coop >/etc/hostname
+cd /tmp
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/interfaces
+mv interfaces /etc/network/
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/sshd_config
+mv sshd_config /etc/ssh/
