6ef6253e3167c8f07f71de534b6af02072959725
Josh Cox <josh@webhosting.coop>
2014-11-24 17:28:00 -0600

    readme

--- movein.sh
+++ movein.sh
@@ -7,5 +7,7 @@ wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/i
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/sshd_config
 mv sshd_config /etc/ssh/
+https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
+mv octo /usr/bin/
 ufw accept 16222
 ufw deny ssh
