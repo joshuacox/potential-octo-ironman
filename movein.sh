28f4d265af16d85830c06ce6f4ad4e7c0d4cf025
Josh Cox <josh@webhosting.coop>
2014-11-25 01:44:35 -0600

    octo updates

--- movein.sh
+++ movein.sh
@@ -3,11 +3,16 @@
 export BRANCH=octo
 hostname octo.webhosting.coop
 echo octo.webhosting.coop >/etc/hostname
+echo 'domain webhosting.coop'>/etc/resolv.conf
+echo 'search webhosting.coop'>>/etc/resolv.conf
+echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
+echo 'nameserver 8.8.4.4' >>/etc/resolv.conf
+cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
-#mv sshd_config /etc/ssh/
+mv sshd_config /etc/ssh/
 wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
 echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
