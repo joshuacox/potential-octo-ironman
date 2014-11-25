f5a05981e2332b10e233e9dc59e28efc36045912
Josh Cox <josh@webhosting.coop>
2014-11-25 10:33:00 -0600

    suppressing output

--- movein.sh
+++ movein.sh
@@ -9,13 +9,13 @@ echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
 echo 'nameserver 8.8.4.4' >>/etc/resolv.conf
 cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces > /dev/null 2>&1
 mv interfaces /etc/network/
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config > /dev/null 2>&1
 mv sshd_config /etc/ssh/
-wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
-echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
-curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
+wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo > /dev/null 2>&1
+echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa > /dev/null 2>&1
+curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash > /dev/null 2>&1
 mv octo /usr/bin/
-ufw accept 16222
-ufw deny ssh
+ufw accept 16222 > /dev/null 2>&1
+ufw deny ssh > /dev/null 2>&1
