3ac7c48a9933594de298966173f6a8ce942e4ced
Josh Cox <josh@webhosting.coop>
2014-11-25 02:41:54 -0600

    astaroth update

--- movein.sh
+++ movein.sh
@@ -3,12 +3,19 @@
 export BRANCH=astaroth
 hostname octo.webhosting.coop
 echo octo.webhosting.coop >/etc/hostname
+echo 'domain webhosting.coop'>/etc/resolv.conf
+echo 'search webhosting.coop'>>/etc/resolv.conf
+echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
+echo 'nameserver 8.8.4.4' >>/etc/resolv.conf
+cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
-#mv sshd_config /etc/ssh/
-https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
+mv sshd_config /etc/ssh/
+wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
+echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
+curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
 mv octo /usr/bin/
 ufw accept 16222
-#ufw deny ssh
+ufw deny ssh
