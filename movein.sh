c41c0b0568a4223975e78c45940af56d8cf3e371
Josh Cox <josh@webhosting.coop>
2014-11-25 00:37:01 -0600

    one dot

--- movein.sh
+++ movein.sh
@@ -4,11 +4,11 @@ export BRANCH=astaroth
 hostname octo.webhosting.coop
 echo octo.webhosting.coop >/etc/hostname
 cd /tmp
-wget https://rawgithubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
-mv sshd_config /etc/ssh/
+#mv sshd_config /etc/ssh/
 https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
 mv octo /usr/bin/
 ufw accept 16222
-ufw deny ssh
+#ufw deny ssh
