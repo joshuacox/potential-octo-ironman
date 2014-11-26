0d87cf941a872b60843c076b6ee66c96708b5828
Josh Cox <josh@webhosting.coop>
2014-11-25 18:20:45 -0600

    buffalo

--- movein.sh
+++ movein.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 # move in to oltorf
-export BRANCH=astaroth
-hostname octo.webhosting.coop
-echo octo.webhosting.coop >/etc/hostname
+export HOST=buffalo
+export HOSTNAME=$BRANCH.webhosting.coop
+hostname $HOSTNAME
+echo $HOSTNAME >/etc/hostname
 echo 'domain webhosting.coop'>/etc/resolv.conf
 echo 'search webhosting.coop'>>/etc/resolv.conf
 echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
 echo 'nameserver 8.8.4.4' >>/etc/resolv.conf
 cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces > /dev/null 2>&1
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$HOST/interfaces
 mv interfaces /etc/network/
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config > /dev/null 2>&1
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$HOST/sshd_config
 mv sshd_config /etc/ssh/
-wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo > /dev/null 2>&1
-echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa > /dev/null 2>&1
-curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash > /dev/null 2>&1
+wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
+echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
+curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
 mv octo /usr/bin/
-ufw accept 16222 > /dev/null 2>&1
-ufw deny ssh > /dev/null 2>&1
+ufw accept 16222
+ufw deny ssh
