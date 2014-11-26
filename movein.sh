36944263ca78fd475f7dca0146df8aa0626d1002
Josh Cox <josh@webhosting.coop>
2014-11-26 11:23:18 -0600

    buffalo

--- movein.sh
+++ movein.sh
@@ -14,9 +14,5 @@ wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$HOST/in
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$HOST/sshd_config
 mv sshd_config /etc/ssh/
-wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
 echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
-mv octo /usr/bin/
-ufw accept 16222
-ufw deny ssh
