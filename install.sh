d9f6e883f2756b18d4f71ad1499bff03b5bac41c
Josh Cox <josh@webhosting.coop>
2015-01-07 14:02:16 -0600

    173

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /root
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/athena172/cloud-config.yaml
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/athena173/cloud-config.yaml
 coreos-install -d /dev/vda -C stable -c /root/cloud-config.yaml
