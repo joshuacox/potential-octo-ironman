a66d53406db95c93f7be8a8356507cf1caee14a1
Josh Cox <josh@webhosting.coop>
2015-01-07 13:58:54 -0600

    install.sh correction

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /root
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/athena/cloud-config.yaml
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/athena171/cloud-config.yaml
 coreos-install -d /dev/vda -C stable -c /root/cloud-config.yaml
