240ca8056e03b57c22128327000d1e1bf919ed4c
Josh Cox <josh 'at' webhosting coop>
2015-01-13 19:50:28 -0600

    saruman

--- README.md
+++ README.md
@@ -3,8 +3,4 @@ potential-octo-ironman
 
 Some simple things to setup octo on oltorf
 
-curl https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/movein.sh | bash
-
-Install Athena (Deis) onto baremetal on oltorf 189
-
-curl https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/athena171/install.sh | sudo bash
+curl https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/movein.sh | bash
--- movein.sh
+++ movein.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # move in to oltorf
-hostname octo.webhosting.coop
-echo octo.webhosting.coop >/etc/hostname
+#hostname octo.webhosting.coop
+#echo octo.webhosting.coop >/etc/hostname
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/interfaces
-mv interfaces /etc/network/
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/master/sshd_config
-mv sshd_config /etc/ssh/
-https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
-mv octo /usr/bin/
-ufw accept 16222
-ufw deny ssh
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/ifcfg-eth0
+mv ifcfg-eth0 /etc/sysconfig/network-scripts/
+#wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
+#mv sshd_config /etc/ssh/
