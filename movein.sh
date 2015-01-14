75d37c55f291f89ad8126e05ba566d855722b4e1
Josh Cox <josh 'at' webhosting coop>
2015-01-13 20:56:17 -0600

    no ssh config yet

--- movein.sh
+++ movein.sh
@@ -5,5 +5,5 @@
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/ifcfg-eth0
 mv ifcfg-eth0 /etc/sysconfig/network-scripts/
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
-mv sshd_config /etc/ssh/
+#wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
+#mv sshd_config /etc/ssh/
