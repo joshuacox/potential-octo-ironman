00158a8befe6c366f738e37b3047ddca79bdfb0f
Josh Cox <josh@webhosting.coop>
2015-03-27 01:17:57 -0500

    ports

--- movein.sh
+++ movein.sh
@@ -44,6 +44,10 @@ curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/octoaddus.
 # chmod +x /usr/bin/octo
 cd /usr/local/bin/
 ln -s /usr/bin/gitreceive ./
+ufw allow 9997 > /dev/null 2>&1
+ufw allow 9200 > /dev/null 2>&1
+ufw allow 9292 > /dev/null 2>&1
+ufw allow 9300 > /dev/null 2>&1
 ufw allow 5665 > /dev/null 2>&1
 ufw allow 16222 > /dev/null 2>&1
 ufw deny ssh > /dev/null 2>&1
