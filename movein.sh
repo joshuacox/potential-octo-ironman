b74764cf0c75366dd1a0e1593bba0858bee2de68
Josh Cox <josh@webhosting.coop>
2015-05-10 18:02:54 -0500

    not quiet

--- movein.sh
+++ movein.sh
@@ -3,8 +3,8 @@
 export BRANCH=mktulu
 hostname mktulu.thalhalla.net
 echo mktulu.thalhalla.net >/etc/hostname
-apt-get update -qq
-apt-get install -y -qq git python-pip wget curl sudo encfs
+apt-get update
+apt-get install -y git python-pip wget curl sudo encfs
 pip install debops ansible netaddr ldap
 cd /tmp
 wget https://rawgithubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
