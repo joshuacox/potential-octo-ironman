afb0efc435e770718c32395e4232037ccc7f9001
Josh Cox <josh@webhosting.coop>
2015-05-11 00:38:05 -0500

    typo

--- movein.sh
+++ movein.sh
@@ -10,7 +10,7 @@ gem intall bundler
 pip install debops ansible netaddr ldap
 cd /tmp
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash > /dev/null 2>&1
-wget https://rawgithubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
 mv sshd_config /etc/ssh/
