aaf2fff14a63fc6672460947d546e0423883a730
Josh Cox <josh@webhosting.coop>
2015-05-10 19:29:12 -0500

    rubydev and build-essential

--- movein.sh
+++ movein.sh
@@ -4,7 +4,8 @@ export BRANCH=mktulu
 hostname mktulu.thalhalla.net
 echo mktulu.thalhalla.net >/etc/hostname
 apt-get update
-apt-get install -y git python-pip wget curl sudo encfs uuid-runtime unzip bzip2 rsync vim fail2ban
+apt-get install -y git python-pip wget curl sudo encfs uuid-runtime unzip bzip2 rsync vim fail2ban rake build-essential ruby-dev
+gem intall bundler
 pip install debops ansible netaddr ldap
 cd /tmp
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash > /dev/null 2>&1
