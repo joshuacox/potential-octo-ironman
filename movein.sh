9380443172363e84ebbde98982e24278485e26b0
Josh Cox <josh@webhosting.coop>
2015-05-12 17:30:09 -0500

    updates

--- movein.sh
+++ movein.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # move in to oltorf
-#apt-get update
-apt-get install -y qemu qemu-kvm libvirt-bin bridge-utils dnsmasq virtinst
-update-rc.d dnsmasq disable
 export BRANCH=zaphod
 export DOMAIN=thalhalla.net
+apt-get update
+apt-get install -y git python-pip wget curl sudo encfs uuid-runtime unzip bzip2 rsync vim fail2ban byobu vim
+apt-get install -y rake build-essential ruby-dev libgecode-dev qemu qemu-kvm libvirt-bin qemu-utils ebtables dnsmasq bridge-utils dnsmasq virtinst
+update-rc.d dnsmasq disable
 hostname $BRANCH.$DOMAIN
 echo $BRANCH.$DOMAIN >/etc/hostname
 echo "domain $DOMAIN">/etc/resolv.conf
@@ -13,8 +14,8 @@ echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
 echo 'nameserver 8.8.4.4' >>/etc/resolv.conf
 echo "#$BRANCH">>/etc/hosts
 echo "10.0.23.88 $BRANCH.$HOSTNAME">>/etc/hosts
-#cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
-cp /etc/resolv.conf /etc/resolv.conf
+# cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
+# cp /etc/resolv.conf /etc/resolv.conf
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces > /dev/null 2>&1
 mv interfaces /etc/network/
@@ -22,9 +23,9 @@ wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/
 mv sshd_config /etc/ssh/
 echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa > /dev/null 2>&1
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash > /dev/null 2>&1
-cd /caboose
+cd /opt
 git clone https://git.openstack.org/openstack-dev/devstack
 cd /usr/local
-wget -c https://dl.bintray.com/mitchellh/packer/packer_0.7.2_linux_amd64.zip
+wget -c https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 cd bin
-unzip ../packer_0.7.2_linux_amd64.zip
+unzip /usr/local/packer_0.7.5_linux_amd64.zip
