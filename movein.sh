c17a483f6a09d1eb61d8f958ad639e14a9ac0c53
Josh Cox <josh@webhosting.coop>
2015-06-04 12:58:20 -0500

    tyr

--- movein.sh
+++ movein.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # move in to oltorf
-export BRANCH=mktulu
-hostname mktulu.thalhalla.net
-echo mktulu.thalhalla.net >/etc/hostname
+export BRANCH=tyr
+hostname tyr.thalhalla.com
+echo tyr.thalhalla.com >/etc/hostname
 apt-get update
 apt-get install -y git python-pip wget curl sudo encfs uuid-runtime unzip bzip2 rsync vim fail2ban byobu vim ntp
 apt-get install -y rake build-essential ruby-dev libgecode-dev qemu-kvm libvirt-bin qemu-utils ebtables dnsmasq
-gem intall bundler
+gem install bundler
 pip install debops ansible netaddr ldap
 cd /tmp
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash > /dev/null 2>&1
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
+wget https://raw.githubusercontent.com/thalhalla/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
+wget https://raw.githubusercontent.com/thalhalla/potential-octo-ironman/$BRANCH/sshd_config
 mv sshd_config /etc/ssh/
