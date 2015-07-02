146920111d081fefec914c25f87e8d74585713dc
Josh Cox <josh@webhosting.coop>
2015-07-02 12:07:53 -0500

    initial

--- movein.sh
+++ movein.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# move in to oltorf
-export BRANCH=ipa2
-hostname ipa2.webhosting.coop
+# move in to
+export BRANCH=4-30-111-153-htx.webhosting.coop
+hostname 4-30-111-153-htx.webhosting.coop
 apt-get update
 apt-get -y install byobu vim fail2ban curl
-echo ipa2.webhosting.coop >/etc/hostname
+echo 4-30-111-153-htx.webhosting.coop >/etc/hostname
 echo 'domain webhosting.coop'>/etc/resolv.conf
 echo 'search webhosting.coop'>>/etc/resolv.conf
 echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
 echo 'nameserver 8.8.4.4' >>/etc/resolv.conf
 echo '#ipa2'>>/etc/hosts
-echo '65.67.51.189 ipa2.webhosting.coop'>>/etc/hosts
+echo '65.67.51.189 4-30-111-153-htx.webhosting.coop'>>/etc/hosts
 cp /etc/resolv.conf /etc/resolvconf/resolv.conf.d/base
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces > /dev/null 2>&1
@@ -37,13 +37,8 @@ add_line_to_FSTAB()
     printf "%s\n" "$LINE_TO_ADD" >> "$FSTAB"
 }
 
-LINE_TO_ADD="65.67.51.187:/mktulu/exports /exports nfs rw,vers=4,addr=65.67.51.187,clientaddr=65.67.51.188 0 0"
+
+LINE_TO_ADD="/dev/vdb1 /exports ext4 rw 0 0"
 check_if_line_exists || add_line_to_FSTAB
 
-LINE_TO_ADD="/dev/vdb1 /var/lib/docker btrfs rw 0 0"
-check_if_line_exists || add_line_to_FSTAB
-
-LINE_TO_ADD="/dev/vdc1 /exports btrfs rw 0 0"
-check_if_line_exists || add_line_to_FSTAB
-
-echo 'You should reboot ipa2.webhosting.coop now'
+echo 'You should reboot 4-30-111-153-htx.webhosting.coop now'
