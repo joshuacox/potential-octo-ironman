bd87ca270a6b535b37c437c36bcb915abef37a68
Josh Cox <josh@webhosting.coop>
2015-10-28 19:38:39 -0500

    dackero

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ potential-octo-ironman
 
 Some simple things to setup octo on oltorf
 
-curl https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/ichabod/movein.sh | bash
+curl https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/dackero/movein.sh | bash
--- movein.sh
+++ movein.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # move in to oltorf
-export BRANCH=ichabod
-hostname octo.webhosting.coop
-echo octo.webhosting.coop >/etc/hostname
+export BRANCH=dackero
+hostname im.deploycoop.com.
+echo im.deploycoop.com. >/etc/hostname
 cd /tmp
 wget https://rawgithubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
 mv sshd_config /etc/ssh/
-https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
-mv octo /usr/bin/
 ufw accept 16222
 ufw deny ssh
