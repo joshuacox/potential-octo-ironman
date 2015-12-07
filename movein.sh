5b7b04572a566a8f63308bb845b90b45acaddd23
Josh Cox <josh@webhosting.coop>
2015-12-07 16:19:56 -0600

    hawk

--- movein.sh
+++ movein.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# move in to oltorf
-export BRANCH=octo
-hostname octo.webhosting.coop
-echo octo.webhosting.coop >/etc/hostname
+# move in to behemoth
+deluser vagrant
+rm -Rf /home/vagrant
+export BRANCH=hawk
+hostname hawk.webhosting.coop
+echo hawk.webhosting.coop >/etc/hostname
 echo 'domain webhosting.coop'>/etc/resolv.conf
 echo 'search webhosting.coop'>>/etc/resolv.conf
 echo 'nameserver 8.8.8.8' >>/etc/resolv.conf
@@ -13,9 +15,7 @@ wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
 mv sshd_config /etc/ssh/
-wget https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
 echo -e  'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
-mv octo /usr/bin/
 ufw accept 16222
 ufw deny ssh
