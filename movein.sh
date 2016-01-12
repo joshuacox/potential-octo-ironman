15eef2a290921bf73ae4c941289227431adf54b2
Josh Cox <josh@webhosting.coop>
2016-01-12 14:16:28 -0600

    ichabod dacksible

--- movein.sh
+++ movein.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # move in to oltorf
 export BRANCH=ichabod
-hostname octo.webhosting.coop
-echo octo.webhosting.coop >/etc/hostname
+hostname ichabod.thalhalla.net
+echo ichabod.thalhalla.net >/etc/hostname
 cd /tmp
 wget https://rawgithubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/interfaces
 mv interfaces /etc/network/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$BRANCH/sshd_config
 mv sshd_config /etc/ssh/
-https://raw.githubusercontent.com/joshuacox/octohost/master/bin/octo
-mv octo /usr/bin/
 ufw accept 16222
 ufw deny ssh
+curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
