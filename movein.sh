cd7e79ed63871df60181770cd34878f312ecaf1f
Josh Cox <josh@webhosting.coop>
2016-02-19 15:31:32 -0600

    first

--- movein.sh
+++ movein.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# move in to oltorf
-#hostname octo.webhosting.coop
-#echo octo.webhosting.coop >/etc/hostname
+# move in to cloudatcost
+echo salamander.monitaur.net >/etc/hostname
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/ifcfg-eth0
-mv ifcfg-eth0 /etc/sysconfig/network-scripts/
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
 mv sshd_config /etc/ssh/
+# debmon sources
+wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
+echo 'deb http://debmon.org/debmon debmon-jessie main' >/etc/apt/sources.list.d/debmon.list
+apt-get update -yqq
+apt-get get install -yqq icinga2 curl wget unzip vim rsync git
+
+# roustabout
+curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
+curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
