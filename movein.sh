e84c74201aeb45b46746d22cde4b611c05099d47
Josh Cox <josh@webhosting.coop>
2016-02-21 09:34:37 -0600

    httpstransport

--- movein.sh
+++ movein.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # move in to cloudatcost
 echo salamander.monitaur.net >/etc/hostname
+apt-get update -yqq
+apt-get get install -yqq apt-transport-https
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
 mv sshd_config /etc/ssh/
@@ -8,7 +10,7 @@ mv sshd_config /etc/ssh/
 wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
 echo 'deb http://debmon.org/debmon debmon-jessie main' >/etc/apt/sources.list.d/debmon.list
 apt-get update -yqq
-apt-get get install -yqq icinga2 curl wget unzip vim rsync git apt-transport-https
+apt-get get install -yqq icinga2 curl wget unzip vim rsync git
 
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
