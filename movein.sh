2173806064dae59a3980803bfc405c88bb8e5c32
Josh Cox <josh@webhosting.coop>
2016-03-01 09:23:14 -0600

    gecko

--- movein.sh
+++ movein.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # move in to cloudatcost
-echo salamander.monitaur.net >/etc/hostname
+echo gecko.monitaur.net >/etc/hostname
 apt-get update -yqq
 apt-get get install -yqq apt-transport-https
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/gecko/sshd_config
 mv sshd_config /etc/ssh/
 # debmon sources
 wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
@@ -15,3 +15,4 @@ apt-get get install -yqq icinga2 curl wget unzip vim rsync git
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
+
