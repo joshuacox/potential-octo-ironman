baa8d3b83d1ada49c2f2bf3f2d2cc9e15a7747e7
Josh Cox <josh@webhosting.coop>
2016-03-01 20:40:22 -0600

    gecko

--- movein.sh
+++ movein.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # move in to cloudatcost
-echo warthog.monitaur.net >/etc/hostname
+echo gecko.monitaur.net >/etc/hostname
 apt-get update -yqq
 apt-get get install -yqq apt-transport-https
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/warthog/sshd_config
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/gecko/sshd_config
 mv sshd_config /etc/ssh/
-# debmon sources
-wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
-echo 'deb http://debmon.org/debmon debmon-jessie main' >/etc/apt/sources.list.d/debmon.list
+# icinga ppa
+add-apt-repository ppa:formorer/icinga
 apt-get update -yqq
-apt-get install -yqq icinga2 curl wget unzip vim rsync git byobu fail2ban
+apt-get install -yqq icinga2 curl wget unzip vim rsync git byobu fail2ban bzip2
 
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
 # Install docker
-bash /usr/local/bin/DebianDockerInstall
+bash /usr/local/bin/UbuntuDockerInstall
 # Key me
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
