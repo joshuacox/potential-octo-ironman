5f75ad0dced89f9d4e67fb89bd153c30f2e7a855
Josh Cox <josh@webhosting.coop>
2016-03-01 18:17:39 -0600

    warthog

--- movein.sh
+++ movein.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # move in to cloudatcost
-echo gecko.monitaur.net >/etc/hostname
+echo warthog.monitaur.net >/etc/hostname
 apt-get update -yqq
 apt-get get install -yqq apt-transport-https
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/gecko/sshd_config
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/warthog/sshd_config
 mv sshd_config /etc/ssh/
 # debmon sources
 wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
@@ -15,7 +15,7 @@ apt-get install -yqq icinga2 curl wget unzip vim rsync git
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
 # Install docker
-bash /usr/local/bin/UbuntuDockerInstall
+bash /usr/local/bin/DebianDockerInstall
 # Key me
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
 
