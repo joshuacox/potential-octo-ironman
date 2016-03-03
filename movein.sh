81c2195d9ebe9ce106fe387ff7014ebcdddf54a8
Josh Cox <josh@webhosting.coop>
2016-03-03 08:55:05 -0600

    jessie

--- movein.sh
+++ movein.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# move in to cloudatcost
-echo warthog.monitaur.net >/etc/hostname
+# move in to a jessie machine
+# this should be branch name
+MY_NAME=jessie
+echo $MY_NAME.monitaur.net >/etc/hostname
+hostname=$MY_NAME
 apt-get update -yqq
 apt-get get install -yqq apt-transport-https netselect-apt
-netselect-apt -ns -t 25 -o /etc/apt/sources.list jessie
+netselect-apt -ns -t 25 -o /etc/apt/sources.list $MY_NAME
 cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/warthog/sshd_config
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
 mv sshd_config /etc/ssh/
 # debmon sources
 wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
