448a4b20139fcba4f15be9435457129fd3c79e1d
Josh Cox <josh@webhosting.coop>
2016-05-11 09:17:57 -0500

    bear

--- movein.sh
+++ movein.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # move in to cloudatcost
-echo salamander.monitaur.net >/etc/hostname
+echo bear.monitaur.net >/etc/hostname
 apt-get update -yqq
+apt-get upgrade -yqq
 apt-get get install -yqq apt-transport-https
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/saruman/sshd_config
@@ -10,11 +11,11 @@ mv sshd_config /etc/ssh/
 wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
 echo 'deb http://debmon.org/debmon debmon-jessie main' >/etc/apt/sources.list.d/debmon.list
 apt-get update -yqq
-apt-get get install -yqq icinga2 curl wget unzip vim rsync git
+apt-get get install -yqq icinga2 curl wget unzip vim rsync git sudo build-essential
 
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
 
 # Install docker
-/usr/local/bin/DebianInstall
+/usr/local/bin/DebianDockerInstall
