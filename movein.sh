fed1a1e4911afe9c2219f38138894b40d8d987e2
Josh Cox <josh@webhosting.coop>
2016-05-21 09:06:41 -0500

    gigenetnotcentarra

--- movein.sh
+++ movein.sh
@@ -7,6 +7,7 @@ hostname $MY_NAME
 apt-get update -yqq
 apt-get install -yqq apt-transport-https netselect-apt
 netselect-apt -ns -t 25 -o /etc/apt/sources.list jessie
+sed -i 's/centarra/gigenet/g' /etc/apt/sources.list
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
 mv sshd_config /etc/ssh/
