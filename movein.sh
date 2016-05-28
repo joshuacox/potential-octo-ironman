9364fe412d7565d8664991cf7d348ea5b1db7cc9
Josh Cox <josh@webhosting.coop>
2016-05-28 11:40:15 -0500

    thalhalla

--- movein.sh
+++ movein.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # move in to cloudatcost
 MY_NAME=loki
-echo $MY_NAME.monitaur.net >/etc/hostname
+echo $MY_NAME.thalhalla.net >/etc/hostname
 hostname $MY_NAME
 apt-get update -yqq
 apt-get get install -yqq apt-transport-https
