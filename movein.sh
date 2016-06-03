44362be261a0db33c478e5f652c1b65c3f909cb7
Josh Cox <josh@webhosting.coop>
2016-06-03 11:17:27 -0500

    connect

--- movein.sh
+++ movein.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # move in to cloudatcost
 MY_NAME=connect
-echo $MY_NAME.webhosting.coop >/etc/hostname
+echo $MY_NAME.bokbot.com >/etc/hostname
 hostname $MY_NAME
 apt-get update -yqq
 apt-get get install -yqq apt-transport-https
