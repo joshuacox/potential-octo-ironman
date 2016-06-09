d2df8370336570848d33786d1980d0cdd880f097
Josh Cox <josh@webhosting.coop>
2016-06-09 16:41:32 -0500

    stretch

--- movein.sh
+++ movein.sh
@@ -6,16 +6,14 @@ echo $MY_NAME.monitaur.net >/etc/hostname
 hostname $MY_NAME
 apt-get update -yqq
 apt-get install -yqq apt-transport-https netselect-apt
-netselect-apt -ns -t 25 -o /etc/apt/sources.list jessie
+netselect-apt -ns -t 25 -o /etc/apt/sources.list testing
 sed -i 's/centarra/gigenet/g' /etc/apt/sources.list
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
 mv sshd_config /etc/ssh/
 # debmon sources
-wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
-echo 'deb http://debmon.org/debmon debmon-jessie main' >/etc/apt/sources.list.d/debmon.list
 apt-get update -yqq
-apt-get install -yqq icinga2 curl wget unzip vim rsync git byobu fail2ban bzip2 sudo build-essential
+apt-get install -yqq curl wget unzip vim rsync git byobu fail2ban bzip2 sudo build-essential
 
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
