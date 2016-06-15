c0149fb4404ccb029725119305388c343fbdd126
Josh Cox <josh@webhosting.coop>
2016-06-15 11:46:07 -0500

    thoth

--- movein.sh
+++ movein.sh
@@ -2,12 +2,11 @@
 # move in to a jessie machine
 # this should be branch name
 MY_NAME=thoth
-echo $MY_NAME.monitaur.net >/etc/hostname
+echo $MY_NAME.thalhalla.net >/etc/hostname
 hostname $MY_NAME
 apt-get update -yqq
 apt-get install -yqq apt-transport-https netselect-apt
 netselect-apt -ns -t 25 -o /etc/apt/sources.list jessie
-sed -i 's/centarra/gigenet/g' /etc/apt/sources.list
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
 mv sshd_config /etc/ssh/
