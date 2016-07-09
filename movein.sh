2c4b2d287e7002bda1dcdcc4293e9cb18924b8ee
Josh Cox <josh@webhosting.coop>
2016-07-08 23:01:14 -0500

    quiet

--- movein.sh
+++ movein.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # move in to cloudatcost
 DEBIAN_FRONTEND=noninteractive
-apt-get update -y &>/dev/null ; \
-apt-get install -y apt-transport-https &>/dev/null ; \
-apt-get upgrade -y &>/dev/null ; \
-apt-get dist-upgrade -y &>/dev/null ; \
+apt-get update -yqq/ &>/dev/null ; \
+apt-get install -yqq/ apt-transport-https &>/dev/null ; \
+apt-get upgrade -yqq/ &>/dev/null ; \
+apt-get dist-upgrade -yqq/ &>/dev/null ; \
 echo 0
-apt-get install -y linux-image-generic-lts-vivid linux-headers-generic-lts-vivid \
+apt-get install -yqq/ linux-image-generic-lts-vivid linux-headers-generic-lts-vivid \
 curl wget unzip vim rsync git byobu fail2ban bzip2 sudo build-essential &>/dev/null
 cd /tmp
 wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
 mv sshd_config /etc/ssh/
 # icinga ppa
-#add-apt-repository -y ppa:formorer/icinga
-#apt-get update -y
-#apt-get upgrade -y
-#apt-get install -y icinga2
+#add-apt-repository -yqq/ ppa:formorer/icinga
+#apt-get update -yqq/
+#apt-get upgrade -yqq/
+#apt-get install -yqq/ icinga2
 
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
