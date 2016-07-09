3b4532931cd55ddb312c78ba19c530a0e6389156
Josh Cox <josh@webhosting.coop>
2016-07-09 08:40:21 -0500

    bahhumbug

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # update at cloudatcost
 export DEBIAN_FRONTEND=noninteractive
-APT=apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq
-$APT update
-$APT install apt-transport-https
-$APT upgrade
-$APT dist-upgrade
-$APT install linux-image-generic-lts-vivid linux-headers-generic-lts-vivid \
+apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq update
+apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq install apt-transport-https
+apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq upgrade
+apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq dist-upgrade
+apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq install linux-image-generic-lts-vivid linux-headers-generic-lts-vivid \
 curl wget unzip vim rsync git byobu fail2ban bzip2 sudo build-essential 
