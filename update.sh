36da99e7ab6e63c3cb1e6b4f1f2ceeb590fc7062
Josh Cox <josh@webhosting.coop>
2016-07-09 08:43:52 -0500

    order

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # update at cloudatcost
 export DEBIAN_FRONTEND=noninteractive
-apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq update
-apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq install apt-transport-https
-apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq upgrade
-apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq dist-upgrade
-apt-get  -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -yqq install linux-image-generic-lts-vivid linux-headers-generic-lts-vivid \
+apt-get -yqq -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew update
+apt-get -yqq -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install apt-transport-https
+apt-get -yqq -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew upgrade
+apt-get -yqq -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew dist-upgrade
+apt-get -yqq -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install linux-image-generic-lts-vivid linux-headers-generic-lts-vivid \
 curl wget unzip vim rsync git byobu fail2ban bzip2 sudo build-essential 
