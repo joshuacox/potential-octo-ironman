93024c26b3bbdf9123ac81a77f990c56c0f49056
Josh Cox <josh@webhosting.coop>
2016-07-10 09:15:00 -0500

    forcearoo

--- movein.sh
+++ movein.sh
@@ -6,14 +6,27 @@ apt-get update -yqq
 apt-get install -yqq apt-transport-https netselect-apt
 netselect-apt -ns -t 25 -o /etc/apt/sources.list jessie
 sed -i 's/centarra/gigenet/g' /etc/apt/sources.list
-cd /tmp
-wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
-mv sshd_config /etc/ssh/
 # debmon sources
 wget -O - http://debmon.org/debmon/repo.key 2>/dev/null | apt-key add -
 echo 'deb http://debmon.org/debmon debmon-jessie main' >/etc/apt/sources.list.d/debmon.list
-apt-get update -yqq
-apt-get install -yqq icinga2 curl wget unzip vim rsync git byobu fail2ban bzip2 sudo build-essential
+
+cd /tmp
+wget -c https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/update.sh
+update () {
+  bash /tmp/update.sh
+}
+COUNTZERO=0
+while [ $COUNTZERO -lt 4  ]
+do
+  if [ ! -f /usr/bin/git ]
+    then update
+  fi
+  COUNTZERO=$[$COUNTZERO+1]
+done
+# SSH
+cd /tmp
+wget https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
+mv sshd_config /etc/ssh/
 
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
