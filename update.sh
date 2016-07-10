66f53911e72d6d8f2d6a29b6b85e16e31079caf7
Josh Cox <josh@webhosting.coop>
2016-07-10 09:21:47 -0500

    -o oops

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # update at cloudatcost
 export DEBIAN_FRONTEND=noninteractive
-apt-get -yqq -o update
-apt-get -yqq -o install apt-transport-https
-apt-get -yqq -o upgrade
-apt-get -yqq -o dist-upgrade
-apt-get -yqq -o install \
+apt-get -yqq update
+apt-get -yqq install apt-transport-https
+apt-get -yqq upgrade
+apt-get -yqq dist-upgrade
+apt-get -yqq install \
 curl wget unzip vim rsync git byobu \
 fail2ban bzip2 sudo build-essential icinga2
