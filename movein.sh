2ad8943787ec67466955fb267f53bccc8873752d
Josh Cox <josh@webhosting.coop>
2017-04-25 16:29:28 -0500

    move

--- movein.sh
+++ movein.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 # move in to xenial
+
 DEBIAN_FRONTEND=noninteractive
 MY_NAME=xenial
+# TMP
 TMP=$(mktemp -d --suffix=updatetmp)
-
 cd ${TMP}
+
 wget -c https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/update.sh
 update () {
   bash ${TMP}//update.sh
 }
 
+# grub defaults
+wget -c https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/grub
+mv grub /etc/default/grub
+update-grub
+
 #wget -c https://raw.githubusercontent.com/joshuacox/potential-octo-ironman/$MY_NAME/sshd_config
 #mv sshd_config /etc/ssh/
 
-cd ${TMP}
 # roustabout
 curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh|bash
+
 # sprunge
 curl -s https://raw.githubusercontent.com/joshuacox/sprunge/master/install.sh |bash
 
 # Install docker
 bash /usr/local/bin/XenialDockerInstall
-# Key me
+
+# Key WHC
 mkdir -p /root/.ssh
 chmod 0600 /root/.ssh
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/myaddus.sh | bash
-rm -Rf ${TMP}
 
 #WHC
 useradd whc
 gpasswd -a whc docker
-# gits
 mkdir -p /home/whc/git
 mkdir -p /home/whc/.ssh
 cp /root/.ssh/authorized_keys /home/whc/.ssh/authorized_keys
 cd /home/whc/git
-
 git clone https://github.com/joshuacox/docker-icinga2.git
 git clone https://github.com/joshuacox/mkmattermost.git
 git clone https://github.com/joshuacox/mkRedmine.git
@@ -45,3 +50,6 @@ cd docker-gitlab;git checkout envfile
 #chown and chmod
 chown -R whc. /home/whc
 chmod 0600 /home/whc/.ssh
+
+# Delete TMP
+rm -Rf ${TMP}
